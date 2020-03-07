d3ecafd022ebbba8966457d0693b9df43fc59e08
melon <meloniusz@hotmail.com>
2020-03-07 23:26:01 +0800

    search query added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component, useState, useEffect } from "react";
 const App = () => {
   //state
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
   //fetch news
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
@@ -13,9 +14,18 @@ const App = () => {
   useEffect(() => {
     fetchNews();
   });
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div>
       <h2>News</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
       ))}
