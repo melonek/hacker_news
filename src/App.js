152439ef2b502ae15a116bdecab3e5a3af3acc30
melon <meloniusz@hotmail.com>
2020-03-07 23:33:22 +0800

    search only works upon users submit and throws desired results

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ const App = () => {
   //state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState("react");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react"
+  );
   //fetch news
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
   };
   useEffect(() => {
     fetchNews();
-  }, [searchQuery]);
+  }, [url]);
 
   const handleChange = e => {
     setSearchQuery(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSumbit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
       </form>
