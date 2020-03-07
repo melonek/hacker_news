bf15be8e05c941978c3b0046a15be40087c11e78
melon <meloniusz@hotmail.com>
2020-03-07 23:16:11 +0800

    hacker news using react hooks

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component, useState, useEffect } from "react";
+
+const App = () => {
+  //state
+  const [news, setNews] = useState([]);
+  //fetch news
+  const fetchNews = () => {
+    fetch("http://hn.algolia.com/api/v1/search?query=react")
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
+  };
+  useEffect(() => {
+    fetchNews();
+  });
+  return (
+    <div>
+      <h2>News</h2>
+      {news.map((n, i) => (
+        <p key={i}>{n.title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
